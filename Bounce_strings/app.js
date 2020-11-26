2f2ef28fc6a5b21cfa0bb47f4fda438b4a04b8f9
mmporong <74720422+mmporong@users.noreply.github.com>
2020-11-26 22:33:11 +0900

    Add files via upload

---
+++ Bounce_strings/app.js
@@ -0,0 +1,14 @@
+class App{
+    constructor() {
+        this.canvas = document.createElement('canvas');
+        document.body.appendChild(this.canvas);
+        this.ctx = this.canvas.getContext('2d');
+
+        this.pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
+
+        window.addEventListener('resize', this.resize.bind(this), false);
+        this.resize();
+    
+    
+    }
+}
