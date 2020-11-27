c96254244530cda6bbb5cb71fefd43af3fab0d2a
mmporong <74720422+mmporong@users.noreply.github.com>
2020-11-27 20:25:23 +0900

    Add files via upload

--- Bounce_strings/app.js
+++ Bounce_strings/app.js
@@ -8,7 +8,25 @@ class App{
 
         window.addEventListener('resize', this.resize.bind(this), false);
         this.resize();
+        
+        window.requestAnimationFrame(this.animate.bind(this));
     
-    
+    }
+
+    resize() {
+        this.stageWidth = document.body.clientWidth;
+        this.stageHeight = document.body.clientHeight;
+
+        this.canvas.width = this.stageWidth * this.pixelRatio;
+        this.canvas.height = this.stageHeight * this.pixelRatio;
+        this.ctx.scale(this.pixelRatio, this.pixelRatio);
+
+    }
+
+    animate() {
+        window.requestAnimationFrame(this.animate.bind(this));
+
+        this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
+        this
     }
 }
