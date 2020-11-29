e6e490cacd3ce9d05aa10c19b06d8d5ba8058634
mmporong <74720422+mmporong@users.noreply.github.com>
2020-11-29 12:52:46 +0900

    Add files via upload

--- Bounce_strings/app.js
+++ Bounce_strings/app.js
@@ -1,3 +1,6 @@
+import {BounceString} from './bouncestrings.js';
+import {Ball} from './ball.js';
+
 class App{
     constructor() {
         this.canvas = document.createElement('canvas');
@@ -6,8 +9,21 @@ class App{
 
         this.pixelRatio = window.devicePixelRatio > 1 ? 2 : 1;
 
+        this.string = [];
+        this.moveX = -5000;
+        this.moveY = -5000;
+        this.isDown = false;
+
+    
         window.addEventListener('resize', this.resize.bind(this), false);
-        this.resize();
+        this.resize(); 
+
+        this.ball = new Ball(this.stageWidth, this.stageHeight, 100, 20);
+        
+        document.addEventListener('pointerdown', this.onDown.bind(this), false);
+        document.addEventListener('pointermove', this.onMove.bind(this), false);
+        document.addEventListener('pointerup', this.onUp.bind(this), false);
+
         
         window.requestAnimationFrame(this.animate.bind(this));
     
@@ -21,13 +37,60 @@ class App{
         this.canvas.height = this.stageHeight * this.pixelRatio;
         this.ctx.scale(this.pixelRatio, this.pixelRatio);
 
+        const xGap = 50;
+        const yGap = 50;
+        const x1 = xGap;
+        const x2 = this.stageWidth - xGap;
+        const total = Math.floor((this.stageHeight - yGap) / yGap);
+
+        this.strings = [];
+
+        for(let i = 0; i < total; i++) {
+            this.strings[i] = new BounceString(
+                {
+                    x1: x1,
+                    y1: i * yGap + yGap,
+                    x2: x2,
+                    y2: i * yGap + yGap,
+                },
+                '#ff5038'
+            )
+        }
     }
 
     animate() {
         window.requestAnimationFrame(this.animate.bind(this));
-
         this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
+
+        if(this.strings.length > 0)  {
+            for (let i = 0; i < this.strings.length; i++) {
+                this.strings[i].animate(this.ctx, this.ball.x, this.ball.y);
+            }
+        }
+
+        this.ball.animate(this.ctx, this.stageWidth, this.stageHeight);
         
+    }
+
+    onDown(e) {
+        this.isDown = true;
+        this.moveX = e.clientX;
+        this.moveY = e.clientY;
+    }
+
+    onMove(e) {
+        if(this.isDown) {
+            this.moveX = e.clientX;
+            this.moveY = e.clientY;
+        }
+
+    }
+
+    onUp(e) {
+        this.isDown = false;
+
+        this.moveX = -5000;
+        this.moveY = -5000;
     }
 }
 
